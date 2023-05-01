4958ba0832e527e28a22c1b0ff672d6f374da1e3
Martin Iturbide <martiniturbide@gmail.com>
2023-04-30 23:07:42 -0500

    1.01
    Compiled with GCC 9 under ArcaOS 5.0.7

--- DLG.C
+++ DLG.C
@@ -3,19 +3,18 @@
 #include <os2.h>
 #include <string.h>
 #include <stdlib.h>
-#include <smslib.h>
 #include <stdio.h>
 
 #define ID_DLG   10
 #define ID_FRAME 11
 #define ID_MENU  12
 
-MRESULT EXPENTRY ClientWinProc (HWND hwnd, USHORT msg, MPARAM mp1, MPARAM mp2);
+MRESULT EXPENTRY ClientWinProc (HWND hwnd, ULONG msg, MPARAM mp1, MPARAM mp2);
 
 HAB   hab;
 CHAR  bug_buf[100];
 
-USHORT cdecl main(void)
+int _cdecl main(void)
 {
     HMQ   hmq;
     QMSG  qmsg;
@@ -29,9 +28,9 @@ USHORT cdecl main(void)
     WinRegisterClass((HAB)hab, (PSZ)szClientClass, (PFNWP)ClientWinProc,
                       CS_SIZEREDRAW | CS_PARENTCLIP | CS_CLIPSIBLINGS, 0);
 
-    hwnd = WinLoadDlg(HWND_DESKTOP, HWND_DESKTOP, NULL, NULL, ID_DLG, NULL);
+    hwnd = WinLoadDlg(HWND_DESKTOP, HWND_DESKTOP, NULLHANDLE, 0, ID_DLG, NULLHANDLE);
 
-    while (WinGetMsg(hab, &qmsg, NULL, 0, 0))
+    while (WinGetMsg(hab, &qmsg, 0, 0, 0))
          WinDispatchMsg(hab, &qmsg);
 
     WinDestroyWindow(hwnd);
@@ -42,7 +41,7 @@ USHORT cdecl main(void)
 
 }
 
-MRESULT EXPENTRY ClientWinProc (HWND hwnd, USHORT msg, MPARAM mp1, MPARAM mp2)
+MRESULT EXPENTRY ClientWinProc (HWND hwnd, ULONG msg, MPARAM mp1, MPARAM mp2)
 {
       HWND hwndSMS;
       HWND hwndMenu;
@@ -55,10 +54,10 @@ MRESULT EXPENTRY ClientWinProc (HWND hwnd, USHORT msg, MPARAM mp1, MPARAM mp2)
               return(WinDefWindowProc(hwnd, msg, mp1, mp2));
               break;
           case WM_USER+1:
-            hwndSMS=WinQueryWindow(hwnd, QW_PARENT,FALSE);
-            hwndMenu=WinLoadMenu(hwndSMS,NULL,ID_MENU);
-            WinSendMsg(hwndSMS, WM_UPDATEFRAME, (ULONG)FCF_MENU, NULL);
-            WinSetWindowPos(hwndSMS,HWND_TOP,50,50,150,150,SWP_MOVE|SWP_SHOW|SWP_SIZE|SWP_ACTIVATE);
+            hwndSMS=WinQueryWindow(hwnd, QW_PARENT);
+            hwndMenu=WinLoadMenu(hwndSMS, NULLHANDLE, ID_MENU);
+            WinSendMsg(hwndSMS, WM_UPDATEFRAME, (MPARAM) FCF_MENU, NULL);
+            WinSetWindowPos(hwndSMS,HWND_TOP,250,150,1100,700,SWP_MOVE|SWP_SHOW|SWP_SIZE|SWP_ACTIVATE);
             break;
           case WM_ERASEBACKGROUND:
             return ((MRESULT)TRUE);
